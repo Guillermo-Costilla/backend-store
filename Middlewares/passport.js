d887c42fc86ee704dd22d4488429cff43eb964a9
Guillermo Costilla <gcostilla96@gmail.com>
2025-01-11 13:57:11 -0300

    Running Backend

--- Middlewares/passport.js
+++ Middlewares/passport.js
@@ -1,28 +1,25 @@
-import User from '../models/User.js';
-import passport from 'passport';
-import {Strategy, ExtractJwt} from 'passport-jwt';
-
+import User from "../models/User.js";
+import passport from "passport";
+import { Strategy, ExtractJwt } from "passport-jwt";
 
 export default passport.use(
-    new Strategy(
-        {
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretOrKey: process.env.SECRET
-        },
-        async(jwt_payload, done) => {
-            try {
-                const user = await User.findOne({_id: jwt_payload.id}, '-password')
+  new Strategy(
+    {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.SECRET || "defaultSecret",
+    },
+    async (jwt_payload, done) => {
+      try {
+        const user = await User.findOne({ _id: jwt_payload.id }, "-password");
 
-                if(user) {
-                    return done(null, user)
-                }else {
-                    return done(null, false)
-                }
-            } catch (error) {
-                console.log(error)
-                return done(error, false)
-            }
+        if (user) {
+          return done(null, user);
         }
-    )
-)
-
+        return done(null, false);
+      } catch (error) {
+        console.error("Error en la autenticación:", error);
+        return done(error, false);
+      }
+    }
+  )
+);
